58dec5fbefbc8878b47d14fca6b451f89ea2f9f7
ignasiusberneo <ignasiusberneo95@gmail.com>
2025-04-22 00:22:31 +0700

    Init project

---
+++ execute/src/App.jsx
@@ -0,0 +1,23 @@
+// import './App.css'
+import Hero from './components/Hero'
+import AboutUs from './components/AboutUs'
+import Skills from './components/Skills'
+import Services from './components/Services'
+import Testimonials from './components/Testimonials'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <>
+      <Hero />
+      <AboutUs />
+      <Skills />
+      <Services />
+      <Testimonials />
+      <Footer />
+    </>
+  )
+}
+
+export default App
